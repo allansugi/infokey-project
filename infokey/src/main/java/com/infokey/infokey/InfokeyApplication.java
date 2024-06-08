f06614fa6ad3b8ab839b949a3e9c2f6ca8a67405
allansugi <allansugianto@outlook.com>
2024-06-08 11:10:42 +1000

    add spring boot backend template

---
+++ infokey/src/main/java/com/infokey/infokey/InfokeyApplication.java
@@ -0,0 +1,13 @@
+package com.infokey.infokey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfokeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfokeyApplication.class, args);
+	}
+
+}
