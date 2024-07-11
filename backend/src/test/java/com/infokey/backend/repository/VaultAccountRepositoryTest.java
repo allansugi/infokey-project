450f5330263558b6e0daada21dd38c5924f66313
allansugi <allansugianto@outlook.com>
2024-07-11 21:41:24 +1000

    update github action & remove unused import

--- backend/src/test/java/com/infokey/backend/repository/VaultAccountRepositoryTest.java
+++ backend/src/test/java/com/infokey/backend/repository/VaultAccountRepositoryTest.java
@@ -5,7 +5,6 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
